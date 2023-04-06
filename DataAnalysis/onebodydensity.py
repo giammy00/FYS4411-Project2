20533f740d845a82d57f300d08ff4ed2c943c3db
giammy00 <74814411+giammy00@users.noreply.github.com>
2023-04-06 14:57:20 +0200

    worked on histogtam xy

--- DataAnalysis/onebodydensity.py
+++ DataAnalysis/onebodydensity.py
@@ -2,14 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 OUTPUT_DIR = "/home/giammi/Desktop/FYS4411-Project1/Outputs"
-# read histogram from binary file
-histfilename = os.path.join(OUTPUT_DIR, "histogram10_5.bin")
-NX = 100
-NY= 100
-NZ= 100
-hist = np.fromfile(histfilename, dtype=np.uint32).reshape((NX, NY, NZ))
-hist_2d = np.sum(hist, axis=0)
-hist_norm = (hist_2d - np.min(hist_2d)) / (np.max(hist_2d) - np.min(hist_2d))
-reshaped_histogram = hist_norm.reshape((25, 4, 25, 4)).sum(axis=(1, 3))
-plt.imshow(reshaped_histogram)
+
+def get_histogram(N, NX=100, NY=100, NZ=100):
+    HIST_TOT = np.zeros((NX,NY,NZ), dtype=np.uint32)
+    threadnums= np.arange(8)
+    for thread in threadnums:
+        fname = os.path.join(OUTPUT_DIR, "histogram"+str(N)+"_"+str(thread)+".bin")
+        hist = np.fromfile(fname, dtype=np.uint32).reshape((NX, NY, NZ))
+        HIST_TOT+=hist
+
+    return HIST_TOT
+#get histogram of position along xy plane, expect this to be symmetrical
+HIST_TOT = get_histogram(10)
+hist_xy = np.sum(HIST_TOT, axis=2)
+#attempt fit to a gaussian:
+log_hist = np.log(hist_xy).reshape(-1)
+#need the distance from each slot to the center
+# Define the size of the matrix and the boundaries
+size = 100
+x_min, x_max, y_min, y_max = -1.5, 1.5, -1.5, 1.5
+# Create a 2D array with x and y coordinates of each slot
+x_coords = np.linspace(x_min, x_max, size)
+y_coords = np.linspace(y_min, y_max, size)
+xx, yy = np.meshgrid(x_coords, y_coords)
+# Compute the distance of each slot from the center of the matrix
+center_x, center_y = (x_max - x_min) / 2, (y_max - y_min) / 2
+r2 = (xx - center_x)**2 + (yy - center_y)**2
+plt.plot(r2, log_hist)
 plt.show()
+#get histogram along z axis:
+#hist_z = np.sum(HIST_TOT, axis=(0,1))
+
